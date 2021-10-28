814db03aec84050f4c50c46a8eae2a505da2cc21
Jamie Piper <jamiepiperjavascript@gmail.com>
2021-10-28 09:57:17 +0100

    action and payload

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const incrementAction = {
   type: "INCREMENT",
 };
 
+const addAction = (number) => {
+  return {
+    type: "ADD",
+    payload: number,
+  };
+};
+
 const reducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return { value: state.value + 1 };
   }
+
+  if (action.type === "ADD") {
+    return { value: state.value + action.payload };
+  }
+
   return state;
 };
 
@@ -17,4 +29,7 @@ const store = createStore(reducer);
 
 console.log(store.getState());
 store.dispatch(incrementAction);
+console.log(store.getState());
+store.dispatch(addAction(10));
+store.dispatch(addAction(5));
 console.log(store.getState());
